de4eccd787871c4c83b62e5e9a9dfe664df986c5
gittyspark13 <sparkthem13@gmail.com>
2026-03-18 04:41:27 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-if (window.location.pathname.startsWith('/app')) {
-  document.getElementById('root').style.display = 'block'
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
